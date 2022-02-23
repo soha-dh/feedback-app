8774ff5355dadf7375bb086840df1026c3593aa5
soha darvish haghjoo <soha.darvish.haghjoo@gmail.com>
2022-02-23 23:42:03 +0330

    3-5

--- src/components/Feedbackitem.jsx
+++ src/components/Feedbackitem.jsx
@@ -1,10 +1,18 @@
+import PropType from 'prop-types';
+import Card from './shared/Card';
+
 function FeedbackItem({ item }) {
   return (
-    <div className="card">
+    // <Card reverse={true}>
+    <Card>
       <div className="num-display">{item.rating}</div>
       <div className="text-display">{item.text}</div>
-    </div>
+    </Card>
   );
 }
+
+FeedbackItem.propType = {
+  item: PropType.object.isRequired,
+};
 
 export default FeedbackItem;
