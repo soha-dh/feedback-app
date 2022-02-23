4b211b2b7e6e5e80be0b52cb01afd84645cf5881
soha darvish haghjoo <soha.darvish.haghjoo@gmail.com>
2022-02-23 20:12:49 +0330

    3-2 start after

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import Header from './components/Header';
+
 function App() {
-  return <h1>Hello from me </h1>;
+  return (
+    <>
+      <Header />
+      <h1>Hello from me </h1>
+    </>
+  );
 }
 export default App;
