771405a0b54fcce8962cda698ae2ab0cc245453e
soha darvish haghjoo <soha.darvish.haghjoo@gmail.com>
2022-02-24 17:59:24 +0330

    4-2

---
+++ src/components/Button.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Button({ children, version, type, isDisabled }) {
+  return (
+    <button type={type} disabled={isDisabled} className={`btn btn-${version}`}>
+      {children}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  version: 'primary',
+  type: 'button',
+  isDesabled: false,
+};
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  version: PropTypes.string,
+  type: PropTypes.string,
+  isDesabled: PropTypes.bool,
+};
+
+export default Button;
